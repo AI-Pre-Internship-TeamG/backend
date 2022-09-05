dbed5c55d72596ce22e7a2350babe60a27837ec7
김연진 <love_homebody@naver.com>
2022-09-06 03:25:12 +0900

    feat: google social login

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ from environments import get_secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SITE_ID = 1
+SITE_ID = 2
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_valcd idation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
--- users/views.py
+++ users/views.py
@@ -22,85 +22,82 @@ BASE_URL = 'http://localhost:8000/'
 GOOGLE_CALLBACK_URI = BASE_URL + 'users/google/callback/'
 
 state = getattr(settings, 'STATE')
-
-class GoogleLogin(APIView):
+    
+def google_login(request):
     """
     Code request
     """
-    def get(self, request):
-        scope = "https://www.googleapis.com/auth/userinfo.email"
-        client_id = getattr(settings, "SOCIAL_AUTH_GOOGLE_CLIENT_ID")
-        return redirect(f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={scope}")
+    scope = "https://www.googleapis.com/auth/userinfo.email"
+    client_id = getattr(settings, "SOCIAL_AUTH_GOOGLE_CLIENT_ID")
+    return redirect(f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={scope}")
 
 
-class GoogleCallback(APIView):
-    def get(request, self):
-        client_id = getattr(settings, "SOCIAL_AUTH_GOOGLE_CLIENT_ID")
-        client_secret = getattr(settings, "SOCIAL_AUTH_GOOGLE_SECRET")
-        code = request.GET.get('code')
-        """
-        받은 Code를 통해 Access Token 요청
-        """
-        param = {
-            'code': code,
-            'client_id': GOOGLE_CLIENT_ID,
-            'client_secret': GOOGLE_CLIENT_SECRET,
-            'redirect_uri': GOOGLE_CALLBACK_URI,
-            'grant_type': 'authorization_code'
-        }
-        token_req = requests.post( "https://oauth2.googleapis.com/token", data=data)
-        token_req_json = token_req.json()
-        error = token_req_json.get("error")
-        if error is not None:
-            raise JSONDecodeError(error)
-        access_token = token_req_json.get('access_token')
-        """
-        Email request - Access Token을 이용해 사용자의 이메일 반환받음
-        """
-        email_req = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
-        email_req_status = email_req.status_code
-        if email_req_status != 200:
-            return JsonResponse({'err_msg': 'failed to get email'}, status=status.HTTP_400_BAD_REQUEST)
-        email_req_json = email_req.json()
-        email = email_req_json.get('email')
+def google_callback(request):
+    client_id = getattr(settings, "SOCIAL_AUTH_GOOGLE_CLIENT_ID")
+    client_secret = getattr(settings, "SOCIAL_AUTH_GOOGLE_SECRET")
+    code = request.GET.get('code')
+    """
+    받은 Code를 통해 Access Token 요청
+    """
+    token_req = requests.post(
+        f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}&state={state}")
+    token_req_json = token_req.json()
+    error = token_req_json.get("error")
+    if error is not None:
+        raise JSONDecodeError(error)
+    access_token = token_req_json.get('access_token')
+    """
+    Email request - Access Token을 이용해 사용자의 이메일 반환받음
+    """
+    email_req = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
+    email_req_status = email_req.status_code
+    if email_req_status != 200:
+        return JsonResponse({'err_msg': 'failed to get email'}, status=status.HTTP_400_BAD_REQUEST)
+    email_req_json = email_req.json()
+    email = email_req_json.get('email')
 
+    """
+    Signup or Signin Request
+    """
+    try:
         """
-        Signup or Signin Request
+        Sign in
         """
-        try:
-            """
-            Sign in
-            """
-            user = User.objects.get(email=email)
-            social_user = SocialAccount.objects.get(user=user)
-            if social_user is None: # 소셜 로그인 회원이 아닌 user
-                return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)
-            if social_user.provider != 'google': # 다른 SNS를 통해 가입된 user
-                return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
-            data = {'access_token': access_token, 'code': code}
-            accept = accept = requests.post(f"{BASE_URL}accounts/google/login/finish/", data=data)
-            if accept_status != 200:
-                return JsonResponse({'err_msg': 'failed to signin'}, status=accept_status)
-            accept_json = accept.json()
-            refresh_token = acceept_json['refresh_token']
-            cache.set(email, refresh_token)
-            return JsonResponse(accept_json)
-        
-        except User.DoseNotExist:
-            """
-            Sign up
-            """
-            data = {'access_token': access_token, 'code': code}
-            accept = requests.post(f"{BASE_URL}accounts/google/login/finish/", data=data)
-            accept_status = accept.status_code
-            if accept_status != 200:
-                return JsonResponse({'err_msg': 'failed to signup'}, status=accept_status)
-            accept_json = accept.json()
-            refresh_token = accept_json['refresh_token']
-            cache.set(email, refresh_token)
-            return JsonResponse(accept_json)
+        user = User.objects.get(email=email)
+        # 기존에 가입된 유저의 Provider가 google이 아니면 에러 발생, 맞으면 로그인
+        social_user = SocialAccount.objects.get(user=user)
+        if social_user is None:
+            return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)
+        if social_user.provider != 'google': # 다른 SNS를 통해 가입된 user
+            return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
+        # 기존에 Google로 가입한 user
+        data = {'access_token': access_token, 'code': code}
+        accept = requests.post(f"{BASE_URL}users/google/login/finish/", data=data)
+        accept_status = accept.status_code
+        if accept_status != 200:
+            return JsonResponse({'err_msg': 'failed to signin'}, status=accept_status)
+        accept_json = accept.json()
+        accept_json.pop('user', None)
+        refresh_token = accept_json['refresh_token']
+        cache.set(email, refresh_token)
+        return JsonResponse(accept_json)
 
-class GoogleLoginToDjango(SocialLoginView):
+    except User.DoesNotExist:
+        """
+        Sign up
+        """
+        data = {'access_token': access_token, 'code': code}
+        accept = requests.post(f"{BASE_URL}users/google/login/finish/", data=data)
+        accept_status = accept.status_code
+        if accept_status != 200:
+            return JsonResponse({'err_msg': 'failed to signup'}, status=accept_status)
+        accept_json = accept.json()
+        accept_json.pop('user', None)
+        refresh_token = accept_json['refresh_token']
+        cache.set(email, refresh_token)
+        return JsonResponse(accept_json)
+
+class GoogleLogin(SocialLoginView):
 
     adapter_class = google_view.GoogleOAuth2Adapter
     callback_url = GOOGLE_CALLBACK_URI
