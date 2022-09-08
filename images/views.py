a5cfe7b35edd39329d00c77f58783753fe478ada
김연진 <love_homebody@naver.com>
2022-09-08 12:07:58 +0900

    feat: extract file extension

--- images/views.py
+++ images/views.py
@@ -16,10 +16,12 @@ class Upload(APIView):
     def post(self, request):
         user = request.user
         uploadFile = request.FILES['filename']
+        # 파일 확장자 추출
+        fileFormat = uploadFile.content_type.split("/")[1]
         uploadFile._set_name(str(uuid.uuid4()))
         s3r = boto3.resource('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key= settings.AWS_SECRET_ACCESS_KEY)
         key = f"before/{uploadFile}" # 사진이 저장될 경로 설정
-        s3r.Bucket(settings.AWS_STORAGE_BUCKET_NAME).put_object( Key=key, Body=uploadFile, ContentType = "png") # 버켓에 이미지 저장
+        s3r.Bucket(settings.AWS_STORAGE_BUCKET_NAME).put_object(Key=key, Body=uploadFile, ContentType=fileFormat) # 버켓에 이미지 저장
         imageUrl = settings.AWS_S3_CUSTOM_DOMAIN+key # 이 뒤로 이미지 처리
         return JsonResponse({"imageUrl": imageUrl})
         ## 받아온 url로 이미지 처리 후 다시 url 값 반환
