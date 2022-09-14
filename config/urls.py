47a2da439a34412aae9f47ed2052003f172cb9a5
김연진 <love_homebody@naver.com>
2022-09-14 17:16:59 +0900

    feat: add prometheus, grafana

--- config/urls.py
+++ config/urls.py
@@ -35,6 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger',cache_timeout=0), name='schema-swagger-ui'),
+    path('', include('django_prometheus.urls')),
     path('api/v1/photos/', include("images.urls")),
     path('api/v1/users/', include("users.urls")),
 ]
