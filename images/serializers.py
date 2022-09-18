e11efb8b5557cca3ecb5f7eb42cb9232233103ee
homebdy <love_homebody@naver.com>
2022-09-19 02:38:15 +0900

    chore: change swagger content

--- images/serializers.py
+++ images/serializers.py
@@ -9,4 +9,16 @@ class ImageSerializer(serializers.ModelSerializer):
 class GetImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ['id', 'user_id', 'url', 'created_at']
+        fields = ['id', 'url']
+
+class ImageBodySerializer(serializers.Serializer):
+    file = serializers.ImageField(help_text='이미지 파일')
+
+class GetImageResponseSerializer(serializers.Serializer):
+    image = GetImageSerializer(read_only=True)
+    page = serializers.IntegerField(help_text='현재 페이지')
+    pages = serializers.IntegerField(help_text='전체 페이지')
+    prev_page = serializers.IntegerField(help_text='이전 페이지')
+    next_page = serializers.IntegerField(help_text='다음 페이지')
+    has_next = serializers.IntegerField(help_text='다음 페이지 유무')
+    has_prev = serializers.IntegerField(help_text='이전 페이지 유무')
