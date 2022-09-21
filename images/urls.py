92008266357876cf5883d32365b652a63def27c5
hyunwoo <ashyonu@gmail.com>
2022-09-21 22:07:00 +0900

    feat: add process url

--- images/urls.py
+++ images/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ImagesView.as_view(), name='imagesView'),
-    path('<int:photo>/', views.History.as_view(), name='imageView')
+    path('<int:photo>/', views.History.as_view(), name='imageView'),
+    path('process/', views.Process.as_view(), name='imageView')
 ]
